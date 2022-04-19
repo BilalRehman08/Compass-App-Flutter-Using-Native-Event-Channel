a146cf8d28d3724593c7e00e0f7d7a3eaa0c0f13
BilalRehman08 <bilalrehman080808@gmail.com>
2022-04-19 16:21:56 +0500

    Event Channel Listner

--- lib/event_channel.dart
+++ lib/event_channel.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
+import 'package:eventchannel/listner.dart';
 import 'package:flutter/material.dart';
+import 'dart:math' as maths;
 
 class EventChannel extends StatefulWidget {
   const EventChannel({Key? key}) : super(key: key);
@@ -8,10 +12,32 @@ class EventChannel extends StatefulWidget {
 }
 
 class _EventChannelState extends State<EventChannel> {
+  StreamSubscription? streamSubscription;
+
+  List<double>? sensorValues;
+
+  @override
+  void initState() {
+    sensorValues = <double>[];
+    streamSubscription = eventData!.listen((event) {
+      setState(() {
+        sensorValues = <double>[event.x, event.y, event.z];
+        print("Values: $sensorValues");
+      });
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    // double angle = maths.atan2(sensorValues![1], sensorValues![0]);
     return Scaffold(
-      body: Center(child: Image.asset('assets/compass.png')),
-    );
+        body:
+            // Transform.rotate(
+            //     angle: maths.pi / 2 - angle,
+            //     child:
+            Center(child: Image.asset('assets/compass.png'))
+        // ),
+        );
   }
 }
---
+++ lib/listner.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/services.dart';
+
+const EventChannel CHANNEL = EventChannel("com.example.eventchannel");
+
+class EventChannelData {
+  // x Axis
+  final double x;
+
+  // y Axis
+  final double y;
+
+  // z Axis
+  final double z;
+
+  double getZ() => this.z;
+
+  EventChannelData(this.x, this.y, this.z);
+
+  @override
+  String toString() => '[EventChannelData (x: $x, y:$y, z:$z)]';
+}
+
+EventChannelData listOfValues(List<double> data) {
+  return EventChannelData(data[0], data[1], data[2]);
+}
+
+Stream<EventChannelData>? magneticEvent;
+
+Stream<EventChannelData>? get eventData {
+  // ignore: prefer_conditional_assignment
+  if (magneticEvent == null) {
+    magneticEvent = CHANNEL
+        .receiveBroadcastStream()
+        .map((event) => listOfValues(event.cast<double>()));
+  }
+
+  return magneticEvent;
+}
